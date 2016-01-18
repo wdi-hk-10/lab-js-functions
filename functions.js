1efc5f9f0bb1ab77012f03732682fb07e4d1f683
Daryl Chu <darylchu@me.com>
2016-01-18 09:04:52 +0800

    completed

--- functions.js
+++ functions.js
@@ -1,54 +1,83 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(x,y) {
+  if (x >= y) {
+  return x;
+  } else {
+    return y;
+  }
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(x,y,z) {
+  if (maxOfTwoNumbers(x,y) <= z) {
+     return z;
+    } else if (maxOfTwoNumbers(x,z) <= y) {
+      return y;
+    } else {
+      return x;
+    }
 }
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(x) {
+  if ((x == "a") || (x == "e") || (x == "i") || (x == "o") || (x == "u")) {
+    return console.log("TRUE")
+  } else {
+    return console.log("I want a goddamn Vowel *****.")
+  }
 }
+
+isCharacterAVowel("x")
 
 // Question 4
-function sumArray() {
- 
+var sum = [1, 2, 3, 4, 5].reduce(sumArray);
+
+function sumArray(a, b) {
+  return a + b;
 }
 
+console.log(sum);
 
 // Question 4
-function multiplyArray() {
+var multi = [1, 2, 3, 4, 5].reduce(multiplyArray);
 
+function multiplyArray(a, b) {
+  return a * b;
 }
+
+console.log(multi);
 
 
 // Question 5
-var numberOfArguments = function(){
-  
-}
-
-
-
-// Question 6
-var reverseString = function (){
-  
+var reverseString = function (x){
+  return x.split('').reverse().join('');
 };
 
+reverseString("!namtaB potS")
+
+// Question 6
+function findLongestWord(string) {
+    var str = string.split(" ");
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+}
+
+findLongestWord("Hello, My name is Daryl. I like pancakes with maple syrup and Chocolate Ice-Cream")
 
 // Question 7
-function findLongestWord () {
-  
+
+function filterLongWords (){
+
 }
 
-
-// Question 8
-function filterLongWords () {
-  
-}
-
+filderLongWords("Hello, My name is Daryl. I like pancakes with maple syrup and Chocolate Ice-Cream")
 
 // Bonus 1
 //??????
@@ -56,6 +85,6 @@ function filterLongWords () {
 
 // Bonus 2
 function charactersOccurencesCount() {
-  
+
 }
 
