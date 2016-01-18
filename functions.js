185f4bde1e48f8790b7d7d24a35f9f3ada039a39
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-01-18 09:14:21 +0800

    lab js

--- functions.js
+++ functions.js
@@ -1,52 +1,80 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(x,y) {
+  if (x>=y) {
+    return x;
+  } else {
+    return y;
+  }
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(x,y,z) {
+  if (maxOfTwoNumbers(x,y)<=z) {
+    return z;
+    } else if (maxOfTwoNumbers(x,z)<=y) {
+        return y;
+    } else {
+    return x;
+    }
 }
+
+function maxOfThree(x,y,z)
+if (x>=y && x>=z) {
+  return x;
+} else if (y>=x && y>=z) {
+  return y;
+} else {
+  return z;
+}
+
+function maxOfThree(x,y,z)
+maxOfTwo
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(x) {
+  if ((x=="a") || (x=="e") || (x=="i") || (x=="o") || (x=="u")) {
+    return "true";
+  }
+  else{
+    return "false";
+  }
 }
 
 // Question 4
-function sumArray() {
- 
+myArray=[1,2,3,4];
+function sumArray(array) {
+ var a = array.reduce(function(x,y) {
+  return x+y;
+ }); return a;
 }
 
-
-// Question 4
-function multiplyArray() {
-
+function multiplyArray(array) {
+ var a = array.reduce(function(x,y) {
+  return x*y;
+ }); return a;
 }
-
 
 // Question 5
-var numberOfArguments = function(){
-  
-}
-
-
-
-// Question 6
-var reverseString = function (){
-  
+var reverseString = function (x){
+ var y = [];
+ for (var i=0, i <= x.length; i++)
+  o.push(s.charAt(x.length-i));
+return o.join('')
 };
 
 
-// Question 7
+// Question 6
 function findLongestWord () {
-  
+  var a = array.reduce(function(x,y) {
+    return a.length > b. length
+  })
 }
+for ()
 
 
-// Question 8
+// Question 7
 function filterLongWords () {
-  
+
 }
 
 
@@ -56,6 +84,6 @@ function filterLongWords () {
 
 // Bonus 2
 function charactersOccurencesCount() {
-  
+
 }
 
